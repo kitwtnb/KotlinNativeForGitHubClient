f99dd823cb26b7c87c4b8440b192ebac51584dde
usayuki <masapp.01a@gmail.com>
2018-12-06 19:43:48 +0900

    added ios project

---
+++ iosClient/iosClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosClient
+//
+//  Created by 石川 雅之 on 2018/12/06.
+//  Copyright © 2018 usayuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
