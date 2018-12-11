31b90fe08c866465a742153dea52e76f84c74b70
Keita Watanabe <Nal3eCh@gmail.com>
2018-12-11 12:51:57 +0900

    delete load repositories

--- KotlinNativeTutorial/ShareCode/src/commonMain/kotlin/com/github/kitwtnb/kotlinnativetutorial/model/api/GitHubApi.kt
+++ KotlinNativeTutorial/ShareCode/src/commonMain/kotlin/com/github/kitwtnb/kotlinnativetutorial/model/api/GitHubApi.kt
@@ -3,16 +3,9 @@ package com.github.kitwtnb.kotlinnativetutorial.model.api
 import com.github.kitwtnb.kotlinnativetutorial.model.data.Repositories
 
 internal interface GithubApi {
-    suspend fun repositories(query: String? = null): Repositories
+    suspend fun repositories(query: String): Repositories
 }
 
 internal class GithubApiImpl(private val apiClient: ApiClient) : GithubApi {
-    override suspend fun repositories(query: String?): Repositories {
-        val param = if (query == null) {
-            null
-        } else {
-            mapOf("q" to query)
-        }
-        return apiClient.get("search/repositories", param)
-    }
+    override suspend fun repositories(query: String): Repositories = apiClient.get("search/repositories", mapOf("q" to query))
 }
